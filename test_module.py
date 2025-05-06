17bc37e9fff50798d09c67c034569dfa87008fc9
KYSaandeepaGopal <143313974+KYSaandeepaGopal@users.noreply.github.com>
2025-05-06 09:29:37 -0400

    Add files via upload

---
+++ test_module.py
@@ -0,0 +1,21 @@
+import unittest
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+import matplotlib
+
+matplotlib.use('Agg')  # Use non-interactive backend for tests
+
+class TestTimeSeriesVisualizer(unittest.TestCase):
+    def test_line_plot(self):
+        fig = draw_line_plot()
+        self.assertIsNotNone(fig)
+
+    def test_bar_plot(self):
+        fig = draw_bar_plot()
+        self.assertIsNotNone(fig)
+
+    def test_box_plot(self):
+        fig = draw_box_plot()
+        self.assertIsNotNone(fig)
+
+if __name__ == "__main__":
+    unittest.main()
